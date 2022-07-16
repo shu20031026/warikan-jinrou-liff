4bce53b6dcb7b193100e2230a3e34212f37e0cb2
shu20031026 <shu20031026@gmail.com>
2022-07-16 15:30:25 +0900

    add: .vscodeファイルを導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,11 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import type { Liff } from "@line/liff";
-import { useState, useEffect } from "react";
+import "../styles/globals.css"
+import type { Liff } from "@line/liff"
+import type { AppProps } from "next/app"
+import { useState, useEffect } from "react"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [liffObject, setLiffObject] = useState<Liff | null>(null);
-  const [liffError, setLiffError] = useState<string | null>(null);
+  const [liffObject, setLiffObject] = useState<Liff | null>(null)
+  const [liffError, setLiffError] = useState<string | null>(null)
 
   // Execute liff.init() when the app is initialized
   useEffect(() => {
@@ -13,25 +13,25 @@ function MyApp({ Component, pageProps }: AppProps) {
     import("@line/liff")
       .then((liff) => liff.default)
       .then((liff) => {
-        console.log("LIFF init...");
+        console.log("LIFF init...")
         liff
           .init({ liffId: process.env.NEXT_PUBLIC_LIFF_ID! })
           .then(() => {
-            console.log("LIFF init succeeded.");
-            setLiffObject(liff);
+            console.log("LIFF init succeeded.")
+            setLiffObject(liff)
           })
           .catch((error: Error) => {
-            console.log("LIFF init failed.");
-            setLiffError(error.toString());
-          });
-      });
-  }, []);
+            console.log("LIFF init failed.")
+            setLiffError(error.toString())
+          })
+      })
+  }, [])
 
   // Provide `liff` object and `liffError` object
   // to page component as property
-  pageProps.liff = liffObject;
-  pageProps.liffError = liffError;
-  return <Component {...pageProps} />;
+  pageProps.liff = liffObject
+  pageProps.liffError = liffError
+  return <Component {...pageProps} />
 }
 
-export default MyApp;
+export default MyApp
